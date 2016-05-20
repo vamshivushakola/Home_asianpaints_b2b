faeebead4691d52640aa4307ac0721bd4f68fcb9
vamshi <vamshi.vshk@gmail.com>
2016-05-20 17:49:32 +0530

    varints

--- asianpaintsb2b/asianpaintsb2btest/gensrc/com/asianpaintsb2b/test/constants/GeneratedAsianpaintsb2bTestConstants.java
+++ asianpaintsb2b/asianpaintsb2btest/gensrc/com/asianpaintsb2b/test/constants/GeneratedAsianpaintsb2bTestConstants.java
@@ -1,7 +1,7 @@
 /*
  * ----------------------------------------------------------------
  * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
- * --- Generated at May 19, 2016 3:58:04 PM                     ---
+ * --- Generated at May 20, 2016 4:10:43 PM                     ---
  * ----------------------------------------------------------------
  */
 package com.asianpaintsb2b.test.constants;
